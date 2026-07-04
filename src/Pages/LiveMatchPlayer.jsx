6d44ab4ab5ce7c3edd47f29c1ea7d54acd645c97
zdogar556 <zainndogar556@gmai.com>
2026-07-04 22:59:41 +0500

    remove the old content of livestreaming

--- src/Pages/LiveMatchPlayer.jsx
+++ src/Pages/LiveMatchPlayer.jsx
@@ -1,48 +1,11 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { getMatchStream } from "../liveApi";
+import React from 'react'
 
-const LivePlayer = () => {
-  const { state } = useLocation();
-  const match = state?.match;
-
-  const [streamUrl, setStreamUrl] = useState([]);
-
-  useEffect(() => {
-    if (!match) return;
-
-    if (match.sources.length > 0) {
-      const source = match.sources[0];
-
-      getMatchStream(source.source, source.id)
-        .then((res) => {
-          console.log(res.data);
-
-          // Change according to API response
-          setStreamUrl(res.data.stream);
-        })
-        .catch(console.error);
-    }
-  }, [match]);
-
-  if (!match) return <h1>No Match Found</h1>;
-
+const LiveMatchPlayer = () => {
   return (
-    <div className="bg-black min-h-screen text-white p-5">
-      <h1 className="text-3xl font-bold mb-5">{match.title}</h1>
-
-      {streamUrl ? (
-        <iframe
-          src={streamUrl.e}
-          title={match.title}
-          className="w-full h-[700px]"
-          allowFullScreen
-        />
-      ) : (
-        <p>Loading Stream...</p>
-      )}
+    <div className='bg-black w-full h-screen'>
+      Live Streaming Page
     </div>
-  );
-};
+  )
+}
 
-export default LivePlayer;
+export default LiveMatchPlayer
