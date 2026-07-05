072b7bcd9815f5916c7e5ddcbe1647db580e9a71
zdogar556 <zainndogar556@gmai.com>
2026-07-05 22:23:23 +0500

    responsive hero section

--- src/Pages/HerorSection.jsx
+++ src/Pages/HerorSection.jsx
@@ -42,28 +42,49 @@ const movie=movies[currentIndex];
   return (
           <div
             key={movie.id}
-            className="h-[500px] w-full bg-cover bg-center rounded-xl object-cover"
+            className=" w-full h-[400px] md:h-[500px] lg:h-[650px]  bg-cover bg-center rounded-xl  "
             style={{
               backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`
             }}
           >
-            <div className="w-full h-full bg-black bg-opacity-50 flex justify-between  ">
-              <div className="w-1/2 p-8 mt-60 mb-2 ">
+            <div 
+            className="w-full h-full bg-black/60 bg-opacity-50 flex flex-col lg:flex-row justify-end lg:justify-between">
+              <div 
+              className="w-full lg:w-1/2  flex flex-col justify-end p-4 sm:p-8 lg:p-10  ">
               <span
-              
-              className="text-md font-semibold text-red-500">★ Trending #{currentIndex+1}</span>
-                <h1 className="text-2xl font-bold text-white mt-2">{movie.title}</h1>
-              <p className="text-gray-300 mt-2">{movie.overview}</p>
-              <div className="flex gap-4 mt-4 text-white">
-                <button className="bg-red-700 px-4 py-2 rounded-full w-48" >Watch</button> 
-                <button className='bg-black px-2 rounded-full' >More Info</button>
-                <button className='bg-gray-600 px-2 rounded-full'>i More Info</button>
+              className="text-md font-semibold text-red-500">
+                ★ Trending #{currentIndex+1}
+                </span>
+                <h1 
+                className="text-2xl sm:text-3xl lg:text-4xl font-bold text-white mt-2 ">
+                  {movie.title}
+                  </h1>
+              <p 
+              className="text-gray-300 mt-2 text-sm sm:text-base line-clamp-3 lg:line-clamp-none">
+                {movie.overview}
+                </p>
+              <div 
+              className="flex flex-wrap gap-3 mt-5 text-white">
+                <button 
+                className="bg-red-700 px-6 py-3 rounded-full lg:w-48 " >
+                  Watch
+                  </button> 
+                <button
+                 className='bg-black px-6  py-3 rounded-full' >
+                  More Info
+                  </button>
+                <button 
+                className='bg-gray-600 px-6  py-3 rounded-full'>
+                  i More Info
+                  </button>
               </div>              
               </div>
+              {/* left Side */}
                {/* Indicators */}
                <div
-               className="w-1/2 flex flex-col items-end  ">
-                <div className="flex gap-2 mt-8 mr-2 w-48 h-96 ">
+               className="w-full lg:w-1/2 flex  lg:flex-col flex-row lg:justify-between justify-evenly  items-end p-4  ">
+                <div 
+                className="flex gap-2 mt-4 lg:mt-8 mr-2 md:flex-wrap justify-end ">
           {movies.map((_, index) => (
             <button
               key={index}
@@ -76,15 +97,16 @@ const movie=movies[currentIndex];
             />
           ))}
         </div>
-        <div className="flex gap-2 justify-end mr-4 w-48 mb-8 ">
+        <div 
+        className="flex gap-2 justify-end  mr-4 w-48 mb-8 ">
           <button
-          className="w-16 h-16 rounded-full bg-gray-600 text-white flex items-center justify-center hover:bg-gray-700"
+          className="w-10 h-10 sm:w-12 sm:h-12 lg:w-16 lg:h-16 rounded-full bg-gray-600 text-white flex items-center justify-center hover:bg-gray-700"
           onClick={()=> setCurrentIndex((prev)=>(prev-1 +movies.length)% movies.length)}
           >
             <FaChevronLeft />
           </button>
           <button
-          className="w-16 h-16 rounded-full bg-gray-600 text-white flex items-center justify-center hover:bg-gray-700"
+          className="w-10 h-10 sm:w-12 sm:h-12 lg:w-16 lg:h-16 rounded-full bg-gray-600 text-white flex items-center justify-center hover:bg-gray-700"
           onClick={()=> setCurrentIndex((prev)=>(prev+1)% movies.length)}
           >
             <FaChevronRight />
