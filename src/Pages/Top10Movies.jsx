3bacb3d0794e64b1ade1d06691eed5aa2c81490d
zdogar556 <zainndogar556@gmai.com>
2026-07-08 01:15:18 +0500

    when click on top10reated poster movie details comes

--- src/Pages/Top10Movies.jsx
+++ src/Pages/Top10Movies.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { getTrendingMovies } from '../movieApi'
-
+import MovieModal from './MovieModal'
 
 const Top10Movies = () => {
     const [trendingMovies, setTrendingMovies] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState(null);
 useEffect(() => {
     getTrendingMovies()
       .then((res) => {
@@ -28,13 +30,17 @@ useEffect(() => {
             <div 
             key={movie.id}
             className='relative min-w-[320px] group'
+            onClick={() => {
+              setSelectedMovie(movie.id);
+              setIsOpen(true);
+            }}
             >
             <span
               className='absolute -left-5 bottom-0 text-[130px] font-extrabold text-white opacity-80 leading-none [-webkit-text-stroke:2px_black] z-0   '
               >{index+1}</span>
               
               <img 
-              className='w-[320px] h-48 object-cover  rounded-lg'
+              className='w-[320px] h-48 object-cover  rounded-lg c '
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
               <div className='absolute inset-0 bg-black/70 flex items-end justify-center just p-3 opacity-0  group-hover:opacity-100 transition duration-300'>
               <h2
@@ -49,6 +55,7 @@ useEffect(() => {
           ))
         }
       </div>
+      <MovieModal movieId={selectedMovie} isOpen={isOpen} onClose={() => setIsOpen(false)} />
     </div>
   )
 }
