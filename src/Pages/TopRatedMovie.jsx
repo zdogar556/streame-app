00173a803af6f728f42dea705bffa5c42d8bfc86
zdogar556 <zainndogar556@gmai.com>
2026-08-02 17:10:03 +0500

    computer button in TopReated Page

--- src/Pages/TopRatedMovie.jsx
+++ src/Pages/TopRatedMovie.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { useState } from 'react'
 import { getTopRatedMovies } from '../movieApi'
-import { useEffect } from 'react'
+import { useEffect, useRef} from 'react'
 import MovieModal from './MovieModal'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const TopRatedMovie = () => {
     const [movies, setMovies] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    const scrollRef=useRef(null)
+
+    const moveRight=()=>{
+      scrollRef.current?.scrollBy({
+        left: 500,
+        behavior: 'smooth'
+      })
+    }
+    const moveLeft=()=>{
+      scrollRef.current?.scrollBy({
+        left: -500,
+        behavior: 'smooth'
+      })
+    }
 
     useEffect(() => {
         getTopRatedMovies()
@@ -20,14 +36,32 @@ const TopRatedMovie = () => {
     })
   return (
     <div className=" mt-8 " >
-        <div className='mt-8'>
+      <div className="flex gap-2" >
+        <div >
         <h1 className='text-xl font-bold text-white'>Top Rated</h1>
         <p
         className='text-gray-400 mt-2 text-sm'
         >
           Highest rated of all time</p>
       </div>
+
+      <div className="flex items-center gap-2">
+        <button 
+        onClick={moveLeft}
+        className="w-8 h-8 rounded-full bg-gray-600 text-white flex items-center justify-center hover:bg-gray-700"> 
+          <FaChevronLeft />
+        </button>
+        <button
+        onClick={moveRight}
+        className="w-8 h-8 rounded-full bg-gray-600 text-white flex items-center justify-center hover:bg-gray-700"
+        >
+          <FaChevronRight />
+        </button>
+      </div>
+      </div>
+        
     <div
+      ref={scrollRef}
       className="flex overflow-x-auto gap-4 scrollbar-hide mt-3 " 
       >
         {
