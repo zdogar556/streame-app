bb75fddc9417cf82f5b9f2accf540081017bb298
zdogar556 <zainndogar556@gmai.com>
2026-08-06 12:40:33 +0500

    computer Scrollable button add in toprated Tv Show page

--- src/Pages/TopTVShow.jsx
+++ src/Pages/TopTVShow.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
+import { useRef } from 'react'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { getTopTVShows } from '../movieApi'
 import TVShow from '../components/TVShow';
 import TVShowModal from './TvShowModal';
@@ -8,6 +10,11 @@ const TopTVShow = () => {
     const [topTVShow, setTopTVShow] = useState([]);
     const [selectTvShow, setSelectTvShow] = useState(null);
     const [isOpen, setIsOpen] = useState(false);
+
+    const scrollRef=useRef(null)
+
+    const moveRight=()=>{scrollRef.current?.scrollBy({left: 500, behavior: 'smooth'})}
+    const moveLeft=()=>{scrollRef.current?.scrollBy({left: -500, behavior: 'smooth'})}
 
     useEffect(() => {
         getTopTVShows()
@@ -21,10 +28,37 @@ const TopTVShow = () => {
     }, [])
   return (
     <div className=" mt-8  " >
-      <h1 className='text-xl font-bold text-white'>Top Rated</h1>
-      <p
-      className='text-gray-400 mt-2 text-sm'>Highest rated of all time</p>
-        <div className='flex overflow-x-auto gap-4 scrollbar-hide mt-3'>
+      <div className='flex gap-2 items-center'>
+        <div>
+          <h1 
+          className='text-xl font-bold text-white'>
+            Top Rated
+            </h1>
+          <p
+          className='text-gray-400 mt-2 text-sm'>Highest rated of all time</p>
+        </div>
+        <div
+            className='flex gap-2 items-center'
+            >
+              <button
+              onClick={moveLeft}
+              className='w-8 h-8 bg-bg-[#08080cb3] text-white flex items-center justify-center rounded-full hover:bg-gray-950 border'
+              >
+              <FaChevronLeft />
+              </button>
+        
+              <button
+              onClick={moveRight}
+              className='w-8 h-8 bg-bg-[#08080cb3] text-white flex items-center justify-center rounded-full hover:bg-gray-950 border'
+              >
+              <FaChevronRight />
+              </button>
+            </div>
+      </div>
+      
+        <div 
+        ref={scrollRef}
+        className='flex overflow-x-auto gap-4 scrollbar-hide mt-3'>
          {
         topTVShow.map((tvShow) => (
           <div className='min-w-[180px]'
@@ -36,7 +70,7 @@ const TopTVShow = () => {
            >
           
             <img
-            className='w-[180] h-72 object-cover rounded-xl'
+            className='w-[180] h-72 object-cover rounded-xl bg-gray-800'
             src={`https://image.tmdb.org/t/p/w500/${tvShow.poster_path}`} alt={tvShow.title} />
             <h2>{tvShow.name}</h2>
             <p>{tvShow.first_air_date? new Date(tvShow.first_air_date).getFullYear() : "N.A" }</p>
