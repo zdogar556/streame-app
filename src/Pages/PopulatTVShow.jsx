38a4a6c62f6a987180a1ef1a77422b8bac16ae18
zdogar556 <zainndogar556@gmai.com>
2026-08-06 12:30:47 +0500

    computer scrolling button add in PopularTVShow Page

--- src/Pages/PopulatTVShow.jsx
+++ src/Pages/PopulatTVShow.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef} from 'react'
 import { useState } from 'react'
 import { getPopularTVShows } from '../movieApi'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import TVShowModal from './TvShowModal'
 
 
@@ -9,6 +10,19 @@ const PopulatTVShow = () => {
     const [selectTvShow, setSelectTvShow] = useState(null);
     const [isOpen, setIsOpen] = useState(false)
 
+    const scrollRef=useRef(null)
+
+    const moveRight=()=>{
+      scrollRef.current?.scrollBy({
+        left: 500, 
+        behavior: 'smooth'
+      })}
+
+    const moveLeft=()=>{
+      scrollRef.current?.scrollBy({
+        left: -500, 
+        behavior: 'smooth'
+      })}
 
     useEffect(() => {
         getPopularTVShows()
@@ -22,10 +36,39 @@ const PopulatTVShow = () => {
     },[])
   return (
 <div className=" mt-8  " >
-      <h1 className='text-xl font-bold text-white'>Popular TV Shows</h1>
+  <div className="flex gap-2">
+    <div>      
+      <h1 
+      className='text-xl font-bold text-white'>
+        Popular TV Shows
+        </h1>
       <p
-      className='text-gray-400 mt-2 text-sm'>Top shows streaming now</p>
-        <div className='flex overflow-x-auto gap-4 scrollbar-hide mt-3'>
+      className='text-gray-400 mt-2 text-sm'>
+        Top shows streaming now
+        </p>
+        </div>
+    <div
+    className='flex gap-2 items-center'
+    >
+      <button
+      onClick={moveLeft}
+      className='w-8 h-8 bg-bg-[#08080cb3] text-white flex items-center justify-center rounded-full hover:bg-gray-950 border'
+      >
+      <FaChevronLeft />
+      </button>
+
+      <button
+      onClick={moveRight}
+      className='w-8 h-8 bg-bg-[#08080cb3] text-white flex items-center justify-center rounded-full hover:bg-gray-950 border'
+      >
+      <FaChevronRight />
+      </button>
+    </div>
+  </div>
+
+        <div 
+        ref={scrollRef}
+        className='flex overflow-x-auto gap-4 scrollbar-hide mt-3'>
          {
         popularTVShows.map((tvShow) => (
           <div className='min-w-[180px]'
@@ -36,7 +79,7 @@ const PopulatTVShow = () => {
             }}
            >
             <img
-            className='w-[180] h-72 object-cover rounded-xl'
+            className='w-[180] h-72 object-cover rounded-xl bg-gray-800'
             src={`https://image.tmdb.org/t/p/w500/${tvShow.poster_path}`} alt={tvShow.title} />
             <h2>{tvShow.name}</h2>
             <p>{tvShow.first_air_date? new Date(tvShow.first_air_date).getFullYear() : "N.A" }</p>
