17fc83473e5fba194b71f51c38fc0773043b994a
zdogar556 <zainndogar556@gmai.com>
2026-08-06 12:17:01 +0500

    left right moveable button add in Trending Tv Show Page

--- src/Pages/TrendingTVShow.jsx
+++ src/Pages/TrendingTVShow.jsx
@@ -1,13 +1,30 @@
 import React, { useEffect } from 'react'
 import { useState, } from 'react'
 import {usEffect} from 'react'
+import { useRef } from 'react'
 import { getTrendingTVShows } from '../movieApi'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import TVShowModal from './TvShowModal'
 
 const TrendingTVShow = () => {
     const [trendingTVShow, setTrendingTVShow] = useState([]);
     const [selectTvShow, setSelectTvShow] = useState(null);
     const [isOpen, setIsOpen] = useState(false)
+
+    const scrollRef=useRef(null)
+
+    const moveRight=()=>{
+      scrollRef.current?.scrollBy({
+        left: 500, 
+        behavior: 'smooth'
+      })}
+
+    const moveLeft=()=>{
+      scrollRef.current?.scrollBy({
+        left: -500, 
+        behavior: 'smooth'
+      })}
+
     useEffect(() => {
         getTrendingTVShows()
           .then((res) => {
@@ -20,8 +37,29 @@ const TrendingTVShow = () => {
     }, [])
   return (
 <div className=" mt-8  " >
+  <div className='flex gap-2'>
+    <div>
       <h1 className='text-xl font-bold text-white'>Trending TV</h1>
-        <div className='flex overflow-x-auto gap-4 scrollbar-hide mt-3'>
+    </div>
+    <div className='flex gap-2 items-center'>
+      <button
+      onClick={moveLeft}
+      className='w-8 h-8 bg-bg-[#08080cb3] text-white flex items-center justify-center rounded-full hover:bg-gray-950 border'
+      >
+        <FaChevronLeft />
+      </button>
+      <button
+      onClick={moveRight}
+      className='w-8 h-8 bg-bg-[#08080cb3] text-white flex items-center justify-center rounded-full hover:bg-gray-950 border'
+      >
+        <FaChevronRight />
+      </button>
+    </div>
+  </div>
+      
+        <div 
+        ref={scrollRef}
+        className='flex overflow-x-auto gap-4 scrollbar-hide mt-3'>
          {
         trendingTVShow.map((tvShow) => (
           <div className='min-w-[180px]'
@@ -32,7 +70,7 @@ const TrendingTVShow = () => {
             }}
            >
             <img
-            className='w-[180] h-72 object-cover rounded-xl'
+            className='w-[180] h-72 object-cover rounded-xl bg-gray-800'
             src={`https://image.tmdb.org/t/p/w500/${tvShow.poster_path}`} alt={tvShow.title} />
             <h2>{tvShow.name}</h2>
             <p>{tvShow.first_air_date? new Date(tvShow.first_air_date).getFullYear() : "N.A" }</p>
